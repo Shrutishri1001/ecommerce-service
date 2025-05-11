7efbbf2e8d6814c976f81c0a093d35f931ba9ac5
Shrutishri1001 <shrutishri1001@gmail.com>
2025-05-11 09:01:22 +0530

    added db connection

--- src/server.js
+++ src/server.js
@@ -1,8 +1,17 @@
-const express=require("express");
-const app=express();
-app.get("/",(req,res)=>{
+require('dotenv').config(); // Add this at the very top
+const express = require("express");
+const connectDB = require("./configs/db");
+const morgan = require("morgan");
+
+const app = express();
+app.use(morgan("dev"));
+
+connectDB();
+
+app.get("/", (req, res) => {
     res.send("E-commerce service backend is running...");
 });
-app.listen(3000,()=>{
+
+app.listen(3000, () => {
     console.log("Server listening to the port 3000.");
 });
